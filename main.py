09eddc0f11807a8109076b6f73f30d83ccc5f263
LASOV <44479103+Weestle@users.noreply.github.com>
2022-10-13 17:39:26 +0300

    erer

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-f = open('D:\Projects_IT\kisscm_hw5\.git\objects\\b2\997545e387bbb8dba64d8209a67950c12546c4')
+f = open("D:\Projects_IT\kisscm_hw5\.git\objects\99\dd77c92f94edac784bd25f48f85862e7ec28ca", "rb")
 
-print(f.readlines())
+
+# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
+# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
+# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
+# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
+# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
+# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
+# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
