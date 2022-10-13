aebfb41d02d83ded11ff74d468d7060c634269f8
LASOV <44479103+Weestle@users.noreply.github.com>
2022-10-13 17:45:35 +0300

    3rwerwer

--- main.py
+++ main.py
@@ -2,9 +2,4 @@ f = open("D:\Projects_IT\kisscm_hw5\.git\objects\99\dd77c92f94edac784bd25f48f858
 
 
 # df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
-# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
-# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
-# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
-# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
-# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
-# df sdfhdsfhsdfgdhjsfgdhjs fgdjsfds
+
